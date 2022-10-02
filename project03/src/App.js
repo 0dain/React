54a8adca163c390ae4bf24a0a40c4b151f2b0d61
0dain <sayou1214@gmail.com>
2022-10-02 20:25:20 +0900

    pj03내용추가

--- project03/src/App.js
+++ project03/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Ex01 from './Example/Ex01';
 import Ex02 from './Example/Ex02';
 import Ex03 from './Example/Ex03';
-
+import Ex04 from './Example/Ex04';
+import Ex05 from './Example/Ex05';
+import Ex06 from './Example/Ex06';
 
 function App() {
 
@@ -16,8 +18,16 @@ function App() {
       {/* <Ex02/> */}
 
       {/* Ex03: useState를 활용한 랜덤 숫자 게임*/}
-      <Ex03/>
+      {/* <Ex03/> */}
 
+      {/* Ex04: Map 함수를 활용한 반복 컴포넌트 생성 */}
+      {/* <Ex04/> */}
+
+      {/* Ex05: Map 함수를 활용한 컬렉션 실습 */}
+      {/* <Ex05/> */}
+
+      {/* Ex06: 미니실습! 댓글 달기 */}
+      <Ex06/>
     </div>
   );
 }
