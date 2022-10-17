a48fdaa5d762cfba6af7e6f2d6502627616ad3ce
0dain <sayou1214@gmail.com>
2022-10-17 16:48:04 +0900

    투두 원하는 목록 삭제 완

--- selfproject/src/components/ToDoItem.js
+++ selfproject/src/components/ToDoItem.js
@@ -3,14 +3,16 @@ import React from 'react'
 const ToDoItem = ({item}) => {
 
 
-    // const deleteToDo=(e)=>{
-    //     e.target.value
-    // }
+    const deleteToDo=(e)=>{
+      console.log(e.target.value)
+       const del=e.target.parentNode
+       del.remove()
+    }
 
   return (
     <div className='todoitem'>
       {item}
-      <button>삭제</button>
+      <button onClick={deleteToDo}>삭제</button>
     </div>
   )
 }
